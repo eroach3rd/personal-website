986bfb697b15046d9dc2b3c7b4acef1d5398c478
eroach3rd <e.roach3rd@gmail.com>
2025-01-21 11:32:54 -0500

    Update page.tsx
    Fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Header from 'app/components/Header'
-import Hero from 'app/components/Hero'
-import About from 'app/components/About'
-import Skills from 'app/components/Skills'
-import Experience from 'app/components/Experience'
-import Projects from 'app/components/Projects'
-import Contact from 'app/components/Contact'
-import Footer from 'app/components/Footer'
+import Header from './component/Header'
+import Hero from './component/Hero'
+import About from './component/About'
+import Skills from './component/Skills'
+import Experience from './component/Experience'
+import Projects from './component/Projects'
+import Contact from './component/Contact'
+import Footer from './component/Footer'
 
 export default function Home() {
   return (
