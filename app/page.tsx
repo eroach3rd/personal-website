d91b29094b290061ebfa2a43621102e308172626
eroach3rd <e.roach3rd@gmail.com>
2025-01-21 11:24:11 -0500

    Update page.tsx
    Fix deploy errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Skills from './components/Skills'
-import Experience from './components/Experience'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import Header from 'app/components/Header'
+import Hero from 'app/components/Hero'
+import About from 'app/components/About'
+import Skills from 'app/components/Skills'
+import Experience from 'app/components/Experience'
+import Projects from 'app/components/Projects'
+import Contact from 'app/components/Contact'
+import Footer from 'app/components/Footer'
 
 export default function Home() {
   return (
